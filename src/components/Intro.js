c7607ca91439357cfa4de5c9d48e4786c84600ac
LEEJAECHUN <leejc831@naver.com>
2023-10-24 17:52:59 +0900

    수정

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // 에디터의 스타일을 불러옵니다.
-import { save01 } from '../api/Intro_api';
+import { save01, Search01 } from '../api/Intro_api';
 
 function Intro() {
   const [isEditing, setIsEditing] = useState(false); // 에디터의 가시성 상태를 저장
@@ -19,6 +19,14 @@ function Intro() {
       save01(introText);
     }
   };
+
+  // 처음 렌더링 시 Search01 함수 호출
+  useEffect(() => {
+    Search01().then((data) => {
+      // 서버에서 받아온 데이터를 처리
+      // 예: setIntroText(data.introText);
+    });
+  }, []); // 빈 배열을 전달하여 컴포넌트 마운트 시 한 번만 실행
 
 
   return (
