92f7825cd2b25105220ca94da2026f65dc94a578
LEEJAECHUN <leejc831@naver.com>
2023-10-24 10:04:12 +0900

    디자인 수정

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Intro() {
   return (
-    <div className='margin-50'>
+    <div className='margin-content'>
       <img src={process.env.PUBLIC_URL + '/logo512.png'} alt="로고 이미지" className="logo" />
       <h2>안녕하세요!</h2>
-      <p class="description">
+      <p className="description">
         React 환경에서 만든 반응형 어플리케이션 입니다.<br/>
         제작자 : LJC <br/>
         깃허브 놀러오세요
       </p> 
-      <a href="https://github.com/LJC0831" target="_blank" rel="noreferrer"><i class="bi bi-github"></i>이동</a>
+      <i className="bi bi-github"></i><a href="https://github.com/LJC0831" target="_blank" rel="noreferrer">이동</a>
     </div>
   );
 }
