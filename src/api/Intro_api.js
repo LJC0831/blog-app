0961a6da4746ff8cecb0fe5501f2caf7c7b4ff71
LEEJAECHUN <leejc831@naver.com>
2023-10-24 17:52:25 +0900

    데이터 받기

--- src/api/Intro_api.js
+++ src/api/Intro_api.js
@@ -15,25 +15,31 @@ export async function save01(content) {
         console.log('내용이 성공적으로 저장되었습니다.');
       } else {
         // 저장 실패
-        console.error('내용 저장에 실패했습니다.');
+        console.error('IntroInsert 내용 저장에 실패했습니다.');
       }
     } catch (error) {
-      console.error('내용 저장 중 오류 발생:', error);
+      console.error('IntroInsert 내용 저장에 실패했습니다.', error);
     }
   }
 
 // 다른 API 요청을 처리하는 함수 추가
-export async function Search01(endpoint) {
+export async function Search01() {
     try {
-      const response = await fetch(`${api}${endpoint}`);
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      } else {
-        throw new Error('데이터 검색에 실패했습니다.');
+        const response = await fetch(`${api}/api/blog/IntroSearch`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          // 성공적으로 저장됨
+          const data = await response.json(); // JSON 데이터를 파싱
+          return data; // 데이터 반환
+        } else {
+          // 저장 실패
+          console.error('Search01 조회실패');
+        }
+      } catch (error) {
+        console.error('Search01 조회실패', error);
       }
-    } catch (error) {
-      console.error('데이터 검색 중 오류 발생:', error);
-      throw error;
-    }
   }
