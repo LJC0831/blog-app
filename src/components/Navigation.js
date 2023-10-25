4e0e47e70752f813338b976fd3973c05bdd8ee58
LEEJAECHUN <leejc831@naver.com>
2023-10-25 16:37:29 +0900

    프로필사진추가

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,6 +8,9 @@ function Navigation() {
     <div className="margin-top-50">
     <nav className="left-nav">
       <ul>
+        <li>
+          <img src={process.env.PUBLIC_URL + '/profile.JPG'} alt="프로필 사진" className="profile-img" />
+        </li>
         <li><a href="/">소개</a></li>
         <li><a href="/board">개발소스(웹)</a></li>
         <li><a href="/board">DB</a></li>
