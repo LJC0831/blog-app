242c7243e43429b01f4bd84c7cc57b3e9941439f
LEEJAECHUN <leejc831@naver.com>
2023-10-26 10:27:22 +0900

    로컬세션 처리

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Header.css';
 import { login01 } from '../api/Header_api';
 
@@ -7,8 +7,16 @@ function Header() {
   const [password, setPassword] = useState('');
   const [isLoginYn, setIsLogin] = useState(false);
 
+  useEffect(() => {
+    // 컴포넌트가 처음 마운트될 때 로컬 세션을 확인하여 로그인 상태 업데이트
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    setIsLogin(isLoggedIn === 'true');
+  }, []);
+
   const openModal = () => {
-    if(isLoginYn){
+    if (isLoginYn) {
+      // 이미 로그인한 경우, 로그아웃 처리
+      localStorage.removeItem('isLoggedIn');
       setIsLogin(false);
     } else {
       setModalOpen(true);
@@ -31,6 +39,7 @@ function Header() {
   const handleLogin = () => {
     login01(password).then((data) => {
       if (data.length > 0) {
+        localStorage.setItem('isLoggedIn', 'true');
         alert('로그인 성공');
         LoginProc();
       } else {
@@ -44,9 +53,9 @@ function Header() {
       <div>
         <span>LJC Developer Blog</span>
         {isLoginYn ? (
-        <span className='account-class' onClick={openModal}>Admin</span>
+          <span className='account-class' onClick={openModal}>Admin</span>
         ) : (
-        <span className='account-class' onClick={openModal}>일반</span>
+          <span className='account-class' onClick={openModal}>일반용</span>
         )}
       </div>
 
@@ -54,8 +63,7 @@ function Header() {
         <div className="modal">
           <div className="modal-content">
             <h2>로그인</h2>
-            <input type="password" placeholder="비밀번호를 입력해주세요."  value={password} onChange={handlePasswordChange}
-            />
+            <input type="password" placeholder="비밀번호를 입력해주세요." value={password} onChange={handlePasswordChange} />
             <button onClick={handleLogin}>로그인</button>
             <button onClick={closeModal}>닫기</button>
           </div>
