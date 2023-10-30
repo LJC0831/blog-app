225ae235230264d6755cbe33bbfb3d9cb49d9867
LEEJAECHUN <leejc831@naver.com>
2023-10-30 14:53:09 +0900

    node.js , mariadb 게시판 추가

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Navigation from './components/Navigation';
 import Intro from './components/Intro'; // 홈 페이지 컴포넌트 import
 import Vuelist from './components/Vuelist';
 import ReactList from './components/ReactList';
+import NodejsList from './components/NodejsList';
+import MariadbList from './components/MariadbList';
 import BoardWrite from './components/BoardWrite';
+
  
 
 function App() {
@@ -21,6 +24,8 @@ function App() {
             <Route path="/" element={<Intro />} />
             <Route path="/board/vue" element={<Vuelist />} />
             <Route path="/board/react" element={<ReactList />} />
+            <Route path="/board/nodejs" element={<NodejsList />} />
+            <Route path="/board/mariadb" element={<MariadbList />} />
             <Route path="/board/:id" element={<BoardWrite />} />
           </Routes>
         </main>
---
+++ src/components/MariadbList.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Search01 } from '../api/BoardList_api';
+import '../css/Vuelist.css';
+import { useNavigate } from 'react-router-dom';
+
+function NodejsList() {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
+  const handleItemClick = (id) => {
+    navigate(`/board/${id}`);
+  };
+
+  useEffect(() => {
+    // 여기에서 게시글 데이터를 가져오는 API 호출 또는 데이터 로딩 로직을 작성
+    // 이 예제에서는 더미 데이터를 사용
+    const fetchData = async () => {
+      try {
+        Search01('mariadb').then((data) => {
+          if(data.length > 0){
+            setPosts(data);
+            setLoading(false);
+          }
+        });
+        setLoading(false);
+      } catch (error) {
+        console.error('데이터를 가져오는 중 오류가 발생했습니다.', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className='margin-content'>
+      <div className="vue-list-container">
+        <h2>MariaDB 작업 관련 게시판입니다.</h2>
+        <ul className="post-list">
+          {posts.map((post) => (
+            <li key={post.id} className="post-item" onClick={() => handleItemClick(post.id)}>
+              <div className="post-title">{post.title}
+              </div>
+              <div className="post-content">{post.privew_content}</div>
+              <div className="post-content">
+                <span className='position-right'>입력일자 : {post.ins_ymdhms}</span>
+              </div>
+          </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default NodejsList;
