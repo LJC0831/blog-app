2622998b6fde07997b5955c269ba455e84319033
LEEJAECHUN <leejc831@naver.com>
2023-11-01 15:34:18 +0900

    새글작성 base64 처리 완료

--- src/api/BoardWrite_api.js
+++ src/api/BoardWrite_api.js
@@ -64,3 +64,34 @@ export async function Search01(board_id) {
         console.error('Search01 조회실패', error);
       }
 }
+
+export async function upload01(html, board_type, board_id) {
+  try {
+      const chunkSize = 1024 * 50; // 각 덩어리의 크기 (예: 1KB * 50)
+      const chunks = [];
+      for (let i = 0; i < html.length; i += chunkSize) {
+        chunks.push(html.slice(i, i + chunkSize));
+      }
+      for (let i = 0; i <chunks.length; i ++) {
+        const isLastChunk = i === chunks.length - 1;
+        const response = await fetch(`${api}/api/blog/board/upload?board_type=${board_type}&board_id=${board_id}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+          body: isLastChunk ? chunks[i] + 'LASTCHUNK' : chunks[i] 
+        });
+        if (response.ok) {
+          if (response.status === 201) {
+            console.log("base64처리중");
+          } else {
+            // 성공적으로 저장됨
+            const data = await response.text(); // JSON 데이터를 파싱
+            return data; // 데이터 반환
+          }
+        } 
+      }
+    } catch (error) {
+      console.log('base64변환중 에러발생');
+    }
+}
