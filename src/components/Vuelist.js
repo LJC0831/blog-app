5b483ce40803c4c5e54f0d154414f2c2466a5389
leejc <leejc831@naver.com>
2023-11-05 19:16:03 +0900

    settime 수정

--- src/components/Vuelist.js
+++ src/components/Vuelist.js
@@ -30,7 +30,7 @@ function VueList() {
               setLoading(false);
             }
           });
-        }, 300);
+        }, 100);
         setLoading(false);
       } catch (error) {
         console.error('데이터를 가져오는 중 오류가 발생했습니다.', error);
